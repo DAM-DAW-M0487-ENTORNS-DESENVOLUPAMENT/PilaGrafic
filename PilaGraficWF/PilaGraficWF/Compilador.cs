f84437ef7221ac0b521c95d36f44a7e3f343b91b
rm-montilivi <a2410051@institutmontilivi.cat>
2025-03-28 09:42:31 +0100

    RutaArxiu

--- PilaGraficWF/PilaGraficWF/Compilador.cs
+++ PilaGraficWF/PilaGraficWF/Compilador.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PilaGraficWF
+{
+    internal static class Compilador
+    {
+
+
+        private string RutaArxiu()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog();
+            return openFileDialog.FileName;
+        }
+    }
+}
